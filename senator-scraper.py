dd5b9d6716b31e53f1af2db9d5208b121ce8a0d1
Rina Easterday <rina.easterday@gmail.com>
2022-05-10 10:26:40 -0700

    scraping bing instead of using an API

--- senator-scraper.py
+++ senator-scraper.py
@@ -1,44 +1,38 @@
 import time
-import json
-from serpapi import GoogleSearch
-import validators 
-import os
-from dotenv import load_dotenv
-load_dotenv()  
+from bing_image_urls import bing_image_urls
+import re
+import time
 
-def senator_scrape():
-    API_KEY = os.environ.get('API')
-
+def image_scraper():
     while True:
         time.sleep(1.0)
         f = open("./senator.txt", "r")
-        line = f.read()
+        keywords = [keyword.strip() for keyword in f.readlines()]
         f.close()
 
-        if line != "" and not validators.url(f"{line}"):
-            params = {
-                "q": f"{line}",
-                "tbm": "isch",
-                "start": 0,
-                "engine": "google",
-                "num": 1,
-                "api_key": API_KEY
-            }
-            try:
-                search = GoogleSearch(params)
-                results = search.get_dict()
-                image_link = json.dumps(results['images_results'][0]["original"], indent=2, ensure_ascii=False)
+        if keywords != [] and not ('url' in keywords[0]):
+            urls = []
+            print(keywords)
+            valid = False
+            for key in keywords:
+                if re.search('[a-zA-Z]', key):
+                    url = bing_image_urls(key + " wikipedia", limit=1)[0]
+                    urls.append({"keyword": key, "url": url})
+                    valid = True
+                else:
+                    print("invalid keyword")
+            
+            if valid == True:
                 with open("./senator.txt", "w") as fr:
-                    fr.truncate()
-                    fr.write("{}".format(eval(image_link)))
+                    fr.write("{}".format(urls))
                     fr.flush()
+                    fr.close()
+                    print("added urls")
                     time.sleep(5)
-                
-            except:
-                print("request failed")
+            else:
+                print("no valid keywords")
         else:
-            print("waiting on new input")
-            continue 
+            print("waiting on keywords")
 
 if __name__ == '__main__':
-    senator_scrape()
+    image_scraper()
